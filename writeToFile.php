0adb15a23b691bf8f63df9e5f072e5eaa601e2c1
Ben Wakefield <relaxingnoose@gmail.com>
2024-01-24 00:03:39 -0500

    Revert "Add files via upload"
    This reverts commit 68bb2e93f6ae6ed22ca40c86f0b9698867b85690.

--- writeToFile.php
+++ writeToFile.php
@@ -1,17 +1,17 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
+// Get the IP information from the client-side
 $ip = $_POST['ip'];
 $isp = $_POST['isp'];
 $country = $_POST['country'];
 $city = $_POST['city'];
 $time = $_POST['time'];
 
+// Create a string with the information
 $infoString = "IP: $ip\nISP: $isp\nCountry: $country\nCity: $city\nTime: $time\n";
 
+// Specify the file path on the server
 $filePath = 'loginfo.txt';
 
+// Write the information to the file
 file_put_contents($filePath, $infoString, FILE_APPEND | LOCK_EX);
 ?>
